ad33337b5faa2cc3b358986574f8f9f8f334556e
JoseCarlosCabelloMoreno <jose.cabello.moreno98dev@gmail.com>
2022-02-09 17:33:31 -0600

    Pasar de una activity a otra

--- app/src/main/java/com/example/bias_list/MainActivity.kt
+++ app/src/main/java/com/example/bias_list/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.bias_list
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bias_list.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.save.setOnClickListener{
+            openDetailActivity()
+        }
+    }
+    private fun openDetailActivity(){
+        val intent=Intent(this,DetailActivity::class.java)
+        startActivity(intent)
     }
 }
